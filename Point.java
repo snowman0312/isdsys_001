68764fed1cc6b3539a140334fc02bf88abe3ad93
ishihara <zt8h-ishr@asahi-net.or.jp>
2017-05-28 05:33:04 +0900

    新規登録

---
+++ Point.java
@@ -0,0 +1,20 @@
+package common.util;
+
+public class Point {
+	public long getX() {
+		return x;
+	}
+	public void setX(long x) {
+		this.x = x;
+	}
+	public long getY() {
+		return y;
+	}
+	public void setY(long y) {
+		this.y = y;
+	}
+	private long x ;
+	private long y ;
+
+
+}
